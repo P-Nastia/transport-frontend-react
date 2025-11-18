45279605512fe05de39df75c48642ae1dae3e0ff
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-11-18 21:00:52 +0200

    Start working app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MainHeader from "./layout/main/MainHeader.tsx";
+
+function App() {
+
+  return (
+    <>
+      <h1>HELLO</h1>
+        <MainHeader/>
+    </>
+  )
+}
+
+export default App
