2cf571db8c57c4eda28dd6a0a12262320cb2301d
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-11-20 20:57:39 +0200

    add country display and create and update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import './App.css'
-import MainHeader from "./layout/main/MainHeader.tsx";
+import {Route, Routes} from "react-router";
+import MainLayout from "./layout/main/MainLayout.tsx";
+import NotFoundPage from "./pages/common/NotFoundPage.tsx";
+import UserHomePage from "./pages/user/userHomePage";
+import RegisterPage from "./pages/account/registerPage";
+import CreateCountryPage from "./pages/country/createCountryPage";
 
 function App() {
 
-  return (
-    <>
-      <h1>HELLO</h1>
-        <MainHeader/>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<UserHomePage/>} />
+                    <Route path={"/register"} element={<RegisterPage/>} />
+                    <Route path={"/countries/create"} element={<CreateCountryPage/>} />
+                </Route>
+
+                <Route path="*" element={<NotFoundPage/>} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
